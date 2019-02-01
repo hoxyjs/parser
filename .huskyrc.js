c8af59884ae045dd0f7d575518adc9dcfef81430
Joel M <hj923@hotmail.com>
2019-02-01 07:52:25 +0000

    change npm command

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    "pre-commit": "npm run lint && npm run lint-configs && npm run test",
-    "pre-push": "npm run lint && npm run lint-confings"
+    "pre-commit": "npm run lint && npm run lint:config && npm run test",
+    "pre-push": "npm run lint && npm run lint:config"
   }
 };
