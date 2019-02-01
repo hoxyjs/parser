691c3d263b7041f1099d33a2eb52ce56856988c3
Joel M <hj923@hotmail.com>
2019-02-01 07:54:11 +0000

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,32 @@
-const path = require("path");
-const TSLintPlugin = require("tslint-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
-  mode: "development",
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.ts$/,
-        loader: "tslint-loader",
-        enforce: "pre",
+        loader: 'tslint-loader',
+        enforce: 'pre',
         options: {
           fix: true,
           tsConfigFile: 'tsconfig.json',
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
