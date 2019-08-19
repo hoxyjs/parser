81d9313db7658bbecd867746fd97f3a2f89eae0a
9oelM <hj923@hotmail.com>
2019-08-19 20:54:07 +0900

    finalize setting up docker

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import to from "await-to-js";
 import dotenv from "dotenv";
 import Excel from "exceljs";
 
@@ -6,39 +7,17 @@ import Course, {ICourse} from "./course";
 
 dotenv.config();
 
-enum CourseInfo {
-    _,
-    Term,
-    AcadCareer,
-    CourseCode,
-    ClassSelection,
-    StartDate,
-    EndDate,
-    Mon,
-    Tue,
-    Wed,
-    Thu,
-    Fri,
-    Sat,
-    Sun,
-    Venue,
-    StartTime,
-    EndTime,
-    CourseTitle,
-    OfferDept,
-    Instructor
-}
-
 (async () => {
-   // const result = await connect()
+    const [err, result] = await to(connect());
+    if (err) { throw new Error(err); }
 
     const workbook = new Excel.Workbook();
-    const workbookFile = await workbook.xlsx.readFile(`${process.env.ROOT_DIR}/src/assets/timetable.xlsx`);
+    const workbookFile = await workbook.xlsx.readFile(`${process.env.DOCKER_ROOT_DIR || process.env.ROOT_DIR}/src/assets/timetable.xlsx`);
     const worksheet = workbook.getWorksheet(1);
 
     worksheet.eachRow(async (row: Excel.Row, rowNumber: number) => {
         if (rowNumber < 10) {
-            const [_, term, acadCareer,courseCode,classSection,startDate,endDate, mon,tue,wed,thu, fri, sat,sun,venue, startTime, endTime, title, offerDept, instructor] : any = row.values;
+            const [_, term, acadCareer, courseCode, classSection, startDate, endDate, mon, tue, wed, thu, fri, sat, sun, venue, startTime, endTime, title, offerDept, instructor]: any = row.values;
 
             const course = new Course({
                 term,
@@ -47,10 +26,15 @@ enum CourseInfo {
                 classSection,
                 title,
                 offerDept,
-                instructor: instructor && instructor.split(";").map((name:string)=>name.trim())
+                instructor: instructor && instructor.split(";").map((name: string) => name.trim()),
             });
 
             await course.save().then(() => console.log("Course created"));
         }
     });
+
+    Course.find({}, function(err, courses) {
+        console.log(courses);
+    });
+    return;
 })();
